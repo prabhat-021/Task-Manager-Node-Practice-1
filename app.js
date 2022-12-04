a8f20f9c973aa785ed9bd806b26c9ce6b7b39028
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-12-05 01:42:12 +0530

    completed

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const task = require("./routes/task");
-const port = 3004;
+const notFound=require("./Middleware/not-found");
+const port = process.env.PORT || 3004;
 
+app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", task);
 
 const url = process.env.MONGO_URI;
+
+app.use(notFound);
 
 const start = async () => {
     try {
